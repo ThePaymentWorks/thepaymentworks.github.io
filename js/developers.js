ed1e15fe6e427a32fc63c58d4c9e9f5f8480a4a5
James Byrne <jamesdesbyrne@gmail.com>
2018-02-15 17:32:55 +0000

    Working on the developers section

---
+++ js/developers.js
@@ -0,0 +1,66 @@
+function setActive (sim) {
+  // Set the active tab
+  elements = document.getElementsByClassName('tab active');
+  [].forEach.call(elements, function(e) { e.classList.toggle('active'); });
+  document.getElementById(sim).classList.toggle('active');
+
+  const source = getSource(sim);
+  this.notebook.setSource(source);
+}
+
+function getSource (sim) {
+  switch(sim) {
+    case 'optimal':
+      return `var request = require('request');
+
+await request({
+  method: 'POST',
+  url: 'https://api.testingpays.com/30d0b8f9c831bcdb2ae501f37e7f48e4/optimal/v1/orders/12345',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer 30d0b8f9c831bcdb2ae501f37e7f48e4'
+  },
+  body: JSON.stringify({
+    totalAmount: '100',
+    currencyCode: 'EUR',
+    merchantRefNum: '12345b'
+  })
+});`
+
+    case 'stripe':
+      return `// Require the Stripe package
+var stripe = require('stripe')('30d0b8f9c831bcdb2ae501f37e7f48e4');
+
+// Point the app at Testing Pays' Stripe charge Sim
+stripe.setHost('api.testingpays.com', 443, 'https');
+stripe._setApiField('basePath', '30d0b8f9c831bcdb2ae501f37e7f48e4/stripe/v1/');
+stripe._prepResources();
+
+// Create a charge, change the total to get a different response
+await stripe.charges.create({
+  amount: 2000,
+  currency: 'usd',
+  source: 'tok_189fPm2eZvKYlo2CEBNBHYzB',
+  // obtained with Stripe.js
+  description: 'Charge for testing.pays@example.com'
+});`
+    default:
+      return `var request = require('request');
+
+await request({
+  method: 'POST',
+  url: 'https://api.testingpays.com/30d0b8f9c831bcdb2ae501f37e7f48e4/paypal/v1/payments/payment',
+  headers: { 'Authorization': 'Bearer 30d0b8f9c831bcdb2ae501f37e7f48e4' },
+  body: JSON.stringify({
+    "intent": "sale",
+    "payer": { "payment_method": "paypal" },
+    "transactions": [{
+      "amount": {
+        "currency": "USD",
+        "total": "1.00"
+      }
+    }]
+  })
+});`;
+  }
+}
