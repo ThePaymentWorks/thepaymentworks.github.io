86020d999c851ea72a7419b571fda5f9c9a7efc8
James Byrne <jamesdesbyrne@gmail.com>
2018-02-15 17:37:13 +0000

    Restructuring getting instructions

--- js/developers.js
+++ js/developers.js
@@ -11,41 +11,16 @@ function setActive (sim) {
 function getSource (sim) {
   switch(sim) {
     case 'optimal':
-      return `var request = require('request');
-
-await request({
-  method: 'POST',
-  url: 'https://api.testingpays.com/30d0b8f9c831bcdb2ae501f37e7f48e4/optimal/v1/orders/12345',
-  headers: {
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer 30d0b8f9c831bcdb2ae501f37e7f48e4'
-  },
-  body: JSON.stringify({
-    totalAmount: '100',
-    currencyCode: 'EUR',
-    merchantRefNum: '12345b'
-  })
-});`
-
+      return getOptimalInstructions();
     case 'stripe':
-      return `// Require the Stripe package
-var stripe = require('stripe')('30d0b8f9c831bcdb2ae501f37e7f48e4');
-
-// Point the app at Testing Pays' Stripe charge Sim
-stripe.setHost('api.testingpays.com', 443, 'https');
-stripe._setApiField('basePath', '30d0b8f9c831bcdb2ae501f37e7f48e4/stripe/v1/');
-stripe._prepResources();
-
-// Create a charge, change the total to get a different response
-await stripe.charges.create({
-  amount: 2000,
-  currency: 'usd',
-  source: 'tok_189fPm2eZvKYlo2CEBNBHYzB',
-  // obtained with Stripe.js
-  description: 'Charge for testing.pays@example.com'
-});`
+      return getStripeInstructions();
     default:
-      return `var request = require('request');
+      return getPaypalInstructions();
+  }
+}
+
+function getPaypalInstructions() {
+  return `var request = require('request');
 
 await request({
   method: 'POST',
@@ -61,6 +36,42 @@ await request({
       }
     }]
   })
-});`;
-  }
+});` 
+}
+
+function getStripeInstructions() {
+  return `// Require the Stripe package
+var stripe = require('stripe')('30d0b8f9c831bcdb2ae501f37e7f48e4');
+
+// Point the app at Testing Pays' Stripe charge Sim
+stripe.setHost('api.testingpays.com', 443, 'https');
+stripe._setApiField('basePath', '30d0b8f9c831bcdb2ae501f37e7f48e4/stripe/v1/');
+stripe._prepResources();
+
+// Create a charge, change the total to get a different response
+await stripe.charges.create({
+  amount: 2000,
+  currency: 'usd',
+  source: 'tok_189fPm2eZvKYlo2CEBNBHYzB',
+  // obtained with Stripe.js
+  description: 'Charge for testing.pays@example.com'
+});`
+}
+
+function getOptimalInstructions() {
+  return `var request = require('request');
+
+await request({
+  method: 'POST',
+  url: 'https://api.testingpays.com/30d0b8f9c831bcdb2ae501f37e7f48e4/optimal/v1/orders/12345',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer 30d0b8f9c831bcdb2ae501f37e7f48e4'
+  },
+  body: JSON.stringify({
+    totalAmount: '100',
+    currencyCode: 'EUR',
+    merchantRefNum: '12345b'
+  })
+});`
 }
