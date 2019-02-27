f83c301e81ecd7e7a012dc9f17f9342d1a851f0a
Máté Marjai <mate@theproduct.works>
2019-02-27 21:15:19 +0000

    Added all the remaining gateways for the supported gateways page

--- js/components/supported-gateways.js
+++ js/components/supported-gateways.js
@@ -12,17 +12,20 @@ var gatewayList = [{
   'name': 'stripe',
   'logo': 'img/logos/gateways/stripe.png',
   'description': 'Stripe is one of the go-to payment API families out there today. It has everything from one-off payments to peer-to-peer transactions.',
-  'tags': ['Card payments', 'Subscriptions', 'Webhooks']
+  'tags': ['Card payments', 'Subscriptions', 'Webhooks'],
+  'link': '/sims/stripe'
 }, {
   'name': 'realex',
   'logo': 'img/logos/gateways/realex-logo.png',
   'description': 'Realex offers a steady API for e-commerce transactions. You can either do a direct integration or use their hosted pages (HPP) solution.',
-  'tags': ['Realex', '3D secure', 'Hosted Pages']
+  'tags': ['Realex', '3D secure', 'Hosted Pages'],
+  'link': '/sims/realex'
 }, {
   'name': 'authipay',
   'logo': 'img/logos/gateways/authipay.png',
   'description': 'Authipay is AIB Merchant Services\' full offering, allowing you to do everything from simple, recurring card and bank transactions.',
-  'tags': ['Hosted Pages', 'Card payments', 'DCC', 'MCC']
+  'tags': ['Hosted Pages', 'Card payments', 'DCC', 'MCC'],
+  'link': '/authipay/docs/welcome'
 }, {
   'name': 'adyen',
   'logo': 'img/logos/gateways/adyen-logo.png',
@@ -33,6 +36,43 @@ var gatewayList = [{
   'logo': 'img/logos/gateways/apco_pay-logo.png',
   'description': 'Hosted payment pages where you have control over the form\'s look and feel as well as the information you ask for on top of the regular payment fields.',
   'tags': ['Authorise', '3D secure']
+}, {
+  'name': 'experian',
+  'logo': 'img/logos/gateways/experian-logo.png',
+  'description': 'Used for know-your-customer (a.k.a. KYC) type queries, ID and address verification.',
+  'tags': ['ID check', 'KYC']
+}, {
+  'name': 'trulioo',
+  'logo': 'img/logos/gateways/trulioo-logo.png',
+  'description': 'Used for know-your-customer (a.k.a. KYC) type queries, ID and address verification.',
+  'tags': ['ID check', 'KYC']
+}, {
+  'name': 'instadebit',
+  'logo': 'img/logos/gateways/instadebit-logo.png',
+  'description': 'Make transactions and payouts both with cards and ACH (Automated Clearing House) for US and Canada based payments.',
+  'tags': ['ACH', 'E-checks', 'Card payments']
+}, {
+  'name': 'neteller',
+  'logo': 'img/logos/gateways/neteller-logo.png',
+  'description': 'Popular E-Wallet solution, used widely especially in the gaming industry',
+  'tags': ['E-Wallet', 'Card payments']
+}, {
+  'name': 'optimal',
+  'logo': 'img/logos/gateways/optimal-logo.png',
+  'description': 'Part of the PaysafeCard API offerings. online payments on a global scale.',
+  'tags': ['E-Commerce', 'Card payments']
+}, {
+  'name': 'paypal',
+  'logo': 'img/logos/gateways/paypal-logo.png',
+  'description': 'PayPal is one of the payment veterans, with customer and merchant accounts, hosted and API card payments',
+  'tags': ['Peer-to-peer', 'Card payments', 'Bank transfer'],
+  'link': '/sims/paypal'
+}, {
+  'name': 'perfect-card',
+  'logo': 'img/logos/gateways/perfect_card-logo.png',
+  'description': 'Virtual cards for all types of e-commerce transactions. Perfect Card can give you a virtual Visa or Mastercard to use on any other site.',
+  'tags': ['Virtual Card', 'Card payments'],
+  'link': '/perfect-card/docs/welcome'
 }];
 
 var SearchBar = function SearchBar(_ref) {
